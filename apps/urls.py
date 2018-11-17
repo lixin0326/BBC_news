edd98954ffd57c150f84d797750006a851bfdbf4
wuhengxin <710318610@qq.com>
2018-11-17 09:33:06 +0800

    分类功能第一次提交

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 
-from apps.home.apis import IndexApi
+from apps.home.apis import IndexApi, ClassifyApi
 
 api = Api(prefix='/api/v1')  # 前缀
 
@@ -11,3 +11,4 @@ def init_api(app):
 
 # 注册路由系统
 api.add_resource(IndexApi, '/home/')
+api.add_resource(ClassifyApi, '/classify/')
