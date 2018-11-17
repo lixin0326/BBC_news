d32587b656fd8ec5aeb81e9110f79f9338b0f637
wuhengxin <710318610@qq.com>
2018-11-17 10:03:10 +0800

    分类功能第四次提交

--- apps/home/apis.py
+++ apps/home/apis.py
@@ -10,25 +10,3 @@ class IndexApi(Resource):
         return 'hello flask'
 
 
-# 分类的接口
-class ClassifyApi(Resource):
-    def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('c_id', type=int, required=True)
-        self.parser.add_argument('page', type=int, default=1)
-        self.parser.add_argument('size', type=int, default=10)
-
-    def get(self, name):
-        args = self.parser.parse_args()
-        c_id = args.get('c_id')
-        news_list_count = News.query.filter(News.c_id == c_id).count()
-        news_list = News.query.filter(News.c_id == c_id).limit(5).offset(0).all()
-        data = {
-            'news': {
-                'news_list_count': news_list_count,
-                'news_list': news_list,
-            },
-
-        }
-
-        return Result.get_success_result(data)
