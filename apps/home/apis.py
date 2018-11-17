a21795f3f85f520e65c4f3b8f3f514a92a36e6ae
wuhengxin <710318610@qq.com>
2018-11-17 10:19:28 +0800

    分类功能第四次提交

--- apps/home/apis.py
+++ apps/home/apis.py
@@ -1,7 +1,7 @@
-from flask_restful import Resource,reqparse
+from flask_restful import Resource, reqparse
 
 from apps.comm.result import Result
-from apps.home.models import User,News
+from apps.home.models import User, News
 
 
 class IndexApi(Resource):
@@ -10,3 +10,25 @@ class IndexApi(Resource):
         return 'hello flask'
 
 
+# 分类的接口
+class ClassifyApi(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('c_id', type=int, required=True)
+        self.parser.add_argument('page', type=int, default=1)
+        self.parser.add_argument('size', type=int, default=10)
+
+    def get(self):
+        args = self.parser.parse_args()
+        c_id = args.get('c_id')
+        news_list_count = News.query.filter(News.c_id == c_id).count()
+        news_list = News.query.filter(News.c_id == c_id).limit(5).offset(0).all()
+        data = {
+            'news': {
+                'news_list_count': news_list_count,
+                'news_list': news_list,
+            },
+
+        }
+
+        return Result.get_success_result(data)
